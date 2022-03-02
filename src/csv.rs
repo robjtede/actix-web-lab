804a427d15205ab779dcf0142545c561fc5b449d
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-03-02 13:27:49 +0000

    experiment with csv-stream crate

--- src/csv.rs
+++ src/csv.rs
@@ -1,138 +1,152 @@
-use std::{convert::Infallible, error::Error as StdError, io};
-
 use actix_web::{
     body::{BodyStream, MessageBody},
     HttpResponse, Responder,
 };
-use bytes::{Bytes, BytesMut};
+use bytes::Bytes;
 use futures_core::Stream;
-use mime::Mime;
-use pin_project_lite::pin_project;
+use futures_util::TryStreamExt;
 use serde::Serialize;
 
-use crate::{
-    buffered_serializing_stream::BufferedSerializingStream,
-    utils::{InfallibleStream, MutWriter},
-};
+/// A buffered CSV serializing body stream.
+///
+/// This has significant memory efficiency advantages over returning an array of CSV rows when
+/// the data set is very large because it avoids buffering the entire response.
+///
+/// # Examples
+/// ```
+/// # use actix_web::Responder;
+/// # use actix_web_lab::respond::CsvResponder;
+/// # use futures_core::Stream;
+/// # use serde::Serialize;
+/// #[derive(Serialize)]
+/// struct Row { a: usize, b: usize }
+///
+/// fn streaming_data_source() -> impl Stream<Item = Row> {
+///     // get item stream from source
+///     # futures_util::stream::empty()
+/// }
+///
+/// async fn handler() -> impl Responder {
+///     let data_stream = streaming_data_source();
+///
+///     csv_stream::WriterBuilder::default()
+///         .build_stream(data_stream)
+///         .into_responder()
+/// }
+/// ```
+pub trait CsvResponder {
+    type MessageBody: MessageBody;
+    fn into_body_stream(self) -> Self::MessageBody;
 
-pin_project! {
-    /// A buffered CSV serializing body stream.
-    ///
-    /// This has significant memory efficiency advantages over returning an array of CSV rows when
-    /// the data set is very large because it avoids buffering the entire response.
-    ///
-    /// # Examples
-    /// ```
-    /// # use actix_web::Responder;
-    /// # use actix_web_lab::respond::Csv;
-    /// # use futures_core::Stream;
-    /// fn streaming_data_source() -> impl Stream<Item = [String; 2]> {
-    ///     // get item stream from source
-    ///     # futures_util::stream::empty()
-    /// }
-    ///
-    /// async fn handler() -> impl Responder {
-    ///     let data_stream = streaming_data_source();
-    ///
-    ///     Csv::new_infallible(data_stream)
-    ///         .into_responder()
-    /// }
-    /// ```
-    pub struct Csv<S> {
-        // The wrapped item stream.
-        #[pin]
-        stream: S,
-    }
+    type Responder: Responder;
+    fn into_responder(self) -> Self::Responder;
 }
 
-impl<S> Csv<S> {
-    /// Constructs a new `Csv` from a stream of rows.
-    pub fn new(stream: S) -> Self {
-        Self { stream }
-    }
-}
-
-impl<S> Csv<S> {
-    /// Constructs a new `Csv` from an infallible stream of rows.
-    pub fn new_infallible(stream: S) -> Csv<InfallibleStream<S>> {
-        Csv::new(InfallibleStream::new(stream))
-    }
-}
-
-impl<S, T, E> Csv<S>
+impl<S> CsvResponder for csv_stream::Stream<S>
 where
-    S: Stream<Item = Result<T, E>>,
-    T: Serialize,
-    E: Into<Box<dyn StdError>> + 'static,
+    S: Stream + 'static,
+    S::Item: Serialize,
 {
+    type MessageBody =
+        BodyStream<futures_util::stream::MapOk<Self, fn(std::vec::Vec<u8>) -> bytes::Bytes>>;
     /// Creates a chunked body stream that serializes as CSV on-the-fly.
-    pub fn into_body_stream(self) -> impl MessageBody {
-        BodyStream::new(self.into_chunk_stream())
+    fn into_body_stream(self) -> Self::MessageBody {
+        BodyStream::new(self.map_ok(Bytes::from))
     }
 
+    type Responder = HttpResponse<Self::MessageBody>;
     /// Creates a `Responder` type with a serializing stream and correct `Content-Type` header.
-    pub fn into_responder(self) -> impl Responder
-    where
-        S: 'static,
-        T: 'static,
-        E: 'static,
-    {
+    fn into_responder(self) -> Self::Responder {
         HttpResponse::Ok()
             .content_type(mime::TEXT_CSV_UTF_8)
             .message_body(self.into_body_stream())
             .unwrap()
     }
-
-    /// Creates a stream of serialized chunks.
-    pub fn into_chunk_stream(self) -> impl Stream<Item = Result<Bytes, E>> {
-        BufferedSerializingStream::new(self.stream, serialize_csv_row)
-    }
-}
-
-impl Csv<Infallible> {
-    /// Returns the CSV MIME type (`text/csv; charset=utf-8`).
-    pub fn mime() -> Mime {
-        mime::TEXT_CSV_UTF_8
-    }
-}
-
-fn serialize_csv_row<T: Serialize>(wrt: &mut MutWriter<BytesMut>, item: &T) -> io::Result<()> {
-    // serialize CSV row to buffer
-    let mut csv_wrt = csv::Writer::from_writer(wrt);
-    csv_wrt.serialize(&item).unwrap();
-    csv_wrt.flush()
 }
 
 #[cfg(test)]
 mod tests {
-    use std::error::Error as StdError;
-
     use actix_web::body;
     use futures_util::stream;
 
     use super::*;
 
+    #[derive(Serialize)]
+    struct Row {
+        a: usize,
+        b: usize,
+    }
+
     #[actix_web::test]
     async fn serializes_into_body() {
-        let ndjson_body = Csv::new_infallible(stream::iter([
-            [123, 456],
-            [789, 12],
-            [345, 678],
-            [901, 234],
-            [456, 789],
-        ]))
-        .into_body_stream();
+        let csv_body = csv_stream::WriterBuilder::default()
+            .build_stream(stream::iter([
+                Row { a: 123, b: 456 },
+                Row { a: 789, b: 12 },
+                Row { a: 345, b: 678 },
+                Row { a: 901, b: 234 },
+                Row { a: 456, b: 789 },
+            ]))
+            .into_body_stream();
 
-        let body_bytes = body::to_bytes(ndjson_body)
-            .await
-            .map_err(Into::<Box<dyn StdError>>::into)
-            .unwrap();
+        let body_bytes = body::to_bytes(csv_body).await.unwrap();
+
+        const EXP_BYTES: &str = "a,b\n\
+        123,456\n\
+        789,12\n\
+        345,678\n\
+        901,234\n\
+        456,789\n";
+
+        assert_eq!(body_bytes, EXP_BYTES);
+    }
+
+    #[actix_web::test]
+    async fn serializes_into_body_without_headers() {
+        let csv_body = csv_stream::WriterBuilder::default()
+            .has_headers(false)
+            .build_stream(stream::iter([
+                Row { a: 123, b: 456 },
+                Row { a: 789, b: 12 },
+                Row { a: 345, b: 678 },
+                Row { a: 901, b: 234 },
+                Row { a: 456, b: 789 },
+            ]))
+            .into_body_stream();
+
+        let body_bytes = body::to_bytes(csv_body).await.unwrap();
 
         const EXP_BYTES: &str = "123,456\n\
         789,12\n\
         345,678\n\
         901,234\n\
         456,789\n";
+
+        assert_eq!(body_bytes, EXP_BYTES);
+    }
+
+    #[actix_web::test]
+    async fn serializes_into_body_with_tabs_and_quotes() {
+        let csv_body = csv_stream::WriterBuilder::default()
+            .quote_style(csv_stream::QuoteStyle::Always)
+            .delimiter(b'\t')
+            .build_stream(stream::iter([
+                Row { a: 123, b: 456 },
+                Row { a: 789, b: 12 },
+                Row { a: 345, b: 678 },
+                Row { a: 901, b: 234 },
+                Row { a: 456, b: 789 },
+            ]))
+            .into_body_stream();
+
+        let body_bytes = body::to_bytes(csv_body).await.unwrap();
+
+        const EXP_BYTES: &str = "\"a\"\t\"b\"\n\
+        \"123\"\t\"456\"\n\
+        \"789\"\t\"12\"\n\
+        \"345\"\t\"678\"\n\
+        \"901\"\t\"234\"\n\
+        \"456\"\t\"789\"\n";
 
         assert_eq!(body_bytes, EXP_BYTES);
     }
