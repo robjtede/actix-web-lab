49da80429bb7cc99260a8cdc12b8801fba30ebc7
Rob Ede <robjtede@icloud.com>
2022-08-06 01:58:40 +0100

    improve example

--- actix-web-lab/examples/sse.rs
+++ actix-web-lab/examples/sse.rs
@@ -1,10 +1,7 @@
-use std::{
-    io,
-    time::{Duration, SystemTime},
-};
+use std::{io, time::Duration};
 
-use actix_web::{get, App, HttpServer, Responder};
-use actix_web_lab::{respond::Html, sse::sse};
+use actix_web::{get, middleware::Logger, App, HttpServer, Responder};
+use actix_web_lab::{extract::Path, respond::Html, sse::sse};
 use time::format_description::well_known::Rfc3339;
 use tokio::time::sleep;
 use tracing::info;
@@ -16,10 +13,19 @@ async fn index() -> impl Responder {
 
 #[get("/countdown")]
 async fn countdown() -> impl Responder {
+    common_countdown(8)
+}
+
+#[get("/countdown/{n:\\d+}")]
+async fn countdown_from(Path((n,)): Path<(u32,)>) -> impl Responder {
+    common_countdown(n.try_into().unwrap())
+}
+
+fn common_countdown(n: i32) -> impl Responder {
     let (sender, sse) = sse();
 
     actix_web::rt::spawn(async move {
-        let mut n = 8;
+        let mut n = n;
 
         while n > 0 {
             if sender
@@ -75,7 +81,9 @@ async fn main() -> io::Result<()> {
         App::new()
             .service(index)
             .service(countdown)
+            .service(countdown_from)
             .service(timestamp)
+            .wrap(Logger::default())
     })
     .workers(1)
     .bind(bind)?
