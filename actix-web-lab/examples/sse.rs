26b63f7f8436da55c1e840f43adde653b5a4385d
Rob Ede <robjtede@icloud.com>
2022-08-06 01:40:53 +0100

    add sse keep-alives

--- actix-web-lab/examples/sse.rs
+++ actix-web-lab/examples/sse.rs
@@ -1,7 +1,11 @@
-use std::{io, time::Duration};
+use std::{
+    io,
+    time::{Duration, SystemTime},
+};
 
 use actix_web::{get, App, HttpServer, Responder};
 use actix_web_lab::sse::sse;
+use tokio::time::sleep;
 use tracing::info;
 
 #[get("/")]
@@ -9,14 +13,52 @@ async fn index() -> impl Responder {
     "index"
 }
 
-#[get("/sse")]
-async fn events() -> impl Responder {
+#[get("/countdown")]
+async fn countdown() -> impl Responder {
     let (sender, sse) = sse();
 
-    let _ = sender.comment("long comment\ninnit").await;
-    let _ = sender.data("long data\ninnit").await;
+    actix_web::rt::spawn(async move {
+        let mut n = 8;
+
+        while n > 0 {
+            if sender
+                .data_with_event("countdown", n.to_string())
+                .await
+                .is_err()
+            {
+                tracing::warn!("client disconnected at {n}; could not send SSE message");
+                break;
+            }
+
+            n -= 1;
+
+            sleep(Duration::from_secs(1)).await;
+        }
+    });
 
     sse.with_retry_duration(Duration::from_secs(10))
+}
+
+#[get("/time")]
+async fn timestamp() -> impl Responder {
+    let (sender, sse) = sse();
+
+    actix_web::rt::spawn(async move {
+        loop {
+            if sender
+                .data_with_event("timestamp", format!("{:?}", SystemTime::now()))
+                .await
+                .is_err()
+            {
+                tracing::warn!("client disconnected; could not send SSE message");
+                break;
+            }
+
+            sleep(Duration::from_secs(10)).await;
+        }
+    });
+
+    sse.with_keep_alive(Duration::from_secs(3))
 }
 
 #[actix_web::main]
@@ -26,9 +68,14 @@ async fn main() -> io::Result<()> {
     let bind = ("127.0.0.1", 8080);
     info!("staring server at http://{}:{}", &bind.0, &bind.1);
 
-    HttpServer::new(|| App::new().service(index).service(events))
-        .workers(1)
-        .bind(bind)?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(countdown)
+            .service(timestamp)
+    })
+    .workers(1)
+    .bind(bind)?
+    .run()
+    .await
 }
