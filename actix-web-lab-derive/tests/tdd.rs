3b01593d25d4a9a55510f5b25c233c14ed08c213
Rob Ede <robjtede@icloud.com>
2022-10-17 14:20:56 +0100

    add body extract tests in derive

--- actix-web-lab-derive/tests/tdd.rs
+++ actix-web-lab-derive/tests/tdd.rs
@@ -1,18 +1,35 @@
-use actix_web::{get, http::Method, web, App, HttpResponse, Responder};
+use actix_web::{
+    http::{Method, StatusCode},
+    post, web, App, HttpResponse, Responder,
+};
 use actix_web_lab_derive::FromRequest;
 
 #[derive(Debug, FromRequest)]
 struct RequestParts {
     method: Method,
     pool: web::Data<u32>,
+    body: String,
+    body2: String,
 }
 
-#[get("/")]
+#[post("/")]
 async fn handler(parts: RequestParts) -> impl Responder {
-    let RequestParts { method, pool, .. } = parts;
+    let RequestParts {
+        method,
+        pool,
+        body,
+        body2,
+        ..
+    } = parts;
+
     let pool = **pool;
 
-    if method == Method::GET && pool == 42 {
+    assert_eq!(body, "foo");
+
+    // assert that body is taken and second attempt to do so will be blank
+    assert_eq!(body2, "");
+
+    if method == Method::POST && pool == 42 {
         HttpResponse::Ok()
     } else {
         eprintln!("method: {method} | pool: {pool}");
@@ -24,7 +41,6 @@ async fn handler(parts: RequestParts) -> impl Responder {
 async fn basic() {
     let srv = actix_test::start(|| App::new().app_data(web::Data::new(42u32)).service(handler));
 
-    let req = srv.get("/");
-    let res = req.send().await.unwrap();
-    assert!(res.status().is_success());
+    let res = srv.post("/").send_body("foo").await.unwrap();
+    assert_eq!(res.status(), StatusCode::OK);
 }
