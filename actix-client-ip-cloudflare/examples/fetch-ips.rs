96cd49d9851515c376c398ddba363c0977fa4fb1
Rob Ede <robjtede@icloud.com>
2026-06-18 08:24:59 +0100

    chore: remove dbg

--- actix-client-ip-cloudflare/examples/fetch-ips.rs
+++ actix-client-ip-cloudflare/examples/fetch-ips.rs
@@ -10,10 +10,14 @@ async fn main() {
 
     let ips = fetch_trusted_cf_ips().await.unwrap();
 
-    dbg!(ips.contains(IpAddr::from([103, 21, 243, 0])));
-    dbg!(ips.contains(IpAddr::from([103, 21, 244, 0])));
-    dbg!(ips.contains(IpAddr::from([103, 21, 245, 0])));
-    dbg!(ips.contains(IpAddr::from([103, 21, 246, 0])));
-    dbg!(ips.contains(IpAddr::from([103, 21, 247, 0])));
-    dbg!(ips.contains(IpAddr::from([103, 21, 248, 0])));
+    for ip in [
+        IpAddr::from([103, 21, 243, 0]),
+        IpAddr::from([103, 21, 244, 0]),
+        IpAddr::from([103, 21, 245, 0]),
+        IpAddr::from([103, 21, 246, 0]),
+        IpAddr::from([103, 21, 247, 0]),
+        IpAddr::from([103, 21, 248, 0]),
+    ] {
+        assert!(ips.contains(ip));
+    }
 }
